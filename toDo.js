11e6906dce542f7dda1a49e1f5dcfbe49b03c9ec
EliveltonSouzaDev <tst.elivelton@gmail.com>
2021-03-09 11:33:41 -0300

    initial commit

---
+++ toDo.js
@@ -0,0 +1,64 @@
+(() => {
+    const criarTarefa = (evento) => {
+
+        evento.preventDefault()
+
+        const lista = document.querySelector('[data-list]')
+        const input = document.querySelector('[data-form-input]')
+        const valor = input.value
+
+        const tarefa = document.createElement('li')
+        tarefa.classList.add('task')
+        const conteudo = `<p class="content">${valor}</p><div>${btn}${btn2}</div>`
+
+        tarefa.innerHTML = conteudo
+        const btn= tarefa.appendChild(BotaoConclui())
+        const btn2= tarefa.appendChild(BotaoDeleta())
+
+        lista.appendChild(tarefa)
+
+        input.value = " "
+    }
+
+    const novaTarefa = document.querySelector('[data-form-button]')
+
+    novaTarefa.addEventListener('click', criarTarefa);
+
+    const BotaoConclui = () => {
+
+        const botaoConclui = document.createElement('button')
+        botaoConclui.classList.add('check-button')
+        botaoConclui.innerText = 'done'
+        botaoConclui.addEventListener('click', concluirTarefa)
+
+        return botaoConclui
+    }
+
+    const concluirTarefa = (evento) => {
+        const botaoConclui = evento.target
+
+        const tarefaCompleta = botaoConclui.parentElement
+
+        tarefaCompleta.classList.toggle('done')
+    }
+
+    const BotaoDeleta = () =>{
+        const botaoDeleta = document.createElement('button')
+        botaoDeleta.classList.add('delete-button')
+        botaoDeleta.innerText = 'delete'
+        botaoDeleta.addEventListener('click', deletarTarefa)
+
+        return botaoDeleta
+    }
+
+   const deletarTarefa = (evento) =>{
+       const botaoDeleta = evento.target
+
+       const tarefaCompleta = botaoDeleta.parentElement
+
+       tarefaCompleta.remove();
+
+       return botaoDeleta
+   }
+
+})();
